b749d734dcb4fccee07c6085e65e734395da6147
MiKoding <mikaioharison038@gmail.com>
2024-02-02 23:09:41 -0300

    update: iteração de post simulando um retorno backend

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,45 @@ import styles from './App.module.css';
 
 import "./Global.css"
 
+//author: {avatar_url: "", name: "", role: ""}
+//publishedAt: Date
+//content: string
+
+const posts = [
+  {
+
+    id: 1,
+    author:{
+      avatarUrl: 'https://avatars.githubusercontent.com/u/88410418?v=4',
+      nameUser: 'Diego Fernandes',
+      role: 'CTO @ Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      {type:  'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+
+    id: 2,
+    author:{
+      avatarUrl: 'https://avatars.githubusercontent.com/u/88410418?v=4',
+      nameUser: 'Mario Otavio',
+      role: 'CTO @ Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      {type:  'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2022-05-10 21:00:00')
+  }
+];
+
+//uso de iteração: fazer uma repetição de algo, iterar um array por exemplo
+//obs: forEach não tem retorno, porém o map devolve algum valor
 function App() {
   return (
     <div>
@@ -13,8 +52,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post author = "Mikaio Yamada" content = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus illo unde officia voluptates eligendi reiciendis aut maiores inventore ipsam recusandae eaque odit dolores, perferendis corporis nesciunt nostrum molestiae aliquid necessitatibus."/>
-          <Post author = "Regivaldo Surez" content = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus illo unde officia voluptates eligendi reiciendis aut maiores inventore ipsam recusandae eaque odit dolores, perferendis corporis nesciunt nostrum molestiae aliquid necessitatibus." />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
          </main>
       </div>
       
